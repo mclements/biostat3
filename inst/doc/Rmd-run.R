1a87883f99652a3191fc9dfed6af00d3e215d3bd
Mark Clements <mark.clements@ki.se>
2019-10-16 23:03:56 +0200

    Makefile for rendering the HTML

---
+++ inst/doc/Rmd-run.R
@@ -0,0 +1,25 @@
+library(rmarkdown)
+library(prettydoc)
+setwd("~/src/R/biostat3/inst/doc")
+
+## files <- paste0("q", c(1:4,6:13,22,23,25,28),".Rmd")
+files <- paste0("q", c(9:13,22,23,25,28),".Rmd")
+## files <- paste0("q", c(8),".Rmd")
+
+rmarkdown::render("index.Rmd", html_pretty())
+
+setwd("labs")
+for (file in files) {
+    rmarkdown::render(file, html_pretty())
+}
+setwd("..")
+
+setwd("solutions")
+for (file in files) {
+    rmarkdown::render(file, html_pretty())
+}
+setwd("..")
+
+
+## setwd("~/src/R/biostat3/inst/doc/solutions")
+## rmarkdown::render("q2.Rmd", "html_document")
